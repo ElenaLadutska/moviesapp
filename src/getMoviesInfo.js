d48b975c7caa5f47405fb9aae54bab4968adc18d
Elena Ladutska <meduskalena@gmail.com>
2023-06-15 01:36:16 +0300

    Updates

--- src/getMoviesInfo.js
+++
@@ -1,9 +0,0 @@
-const getMoviesInfo = async(cityIndex, setData, setLoading) => {
-  fetch(`https://soft.silverscreen.by:8443/wssite/webapi/event/data?filter=%7B%22city%22:${cityIndex}%7D&extended=true`)
-    .then(response => response.json())
-    .then(data => setData(data))
-    .then(() => setLoading(false))
-    .catch(error => console.log(error))
-};
-
-export default getMoviesInfo;
